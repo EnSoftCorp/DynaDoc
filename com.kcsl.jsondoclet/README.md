7a2780b9493d841986b147d794c30c7d59e889db
Ahmed Tamrawi <ahmedtamrawi@ensoftcorp.com>
2018-07-13 02:36:58 +0300

    Fixing JAR dependecy for the doclet class

---
+++ com.kcsl.jsondoclet/README.md
@@ -0,0 +1,9 @@
+# JSON Doclet
+
+This project contains a custom Java Doc Doclet, that will process a project and outputs a list of JSON files for each class in the process project.
+
+Make sure to copy the contents of the jar files into the bin directory so that the JavaDoc tool can find the dependent jars.
+
+Use the following command line in the directory containing the project to run the documentation generation:
+
+find . -type f -name "*.java" | xargs javadoc -private -doclet com.kcsl.doclet.JSONDoclet -docletpath "absolute path to doclet project bin folder"
--- com.kcsl.jsondoclet/src/com/kcsl/doclet/JSONDoclet.java
+++ com.kcsl.jsondoclet/src/com/kcsl/doclet/JSONDoclet.java
@@ -1,13 +1,12 @@
 package com.kcsl.doclet;
 
 import java.io.*;
+import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.*;
-
-import javax.json.Json;
-import javax.json.JsonWriter;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.json.JSONObject;
 
 import com.sun.javadoc.*;
 
@@ -16,28 +15,42 @@ public final class JSONDoclet {
 	private static final String OUTPUT_DIRECTORY_LOCATION = "/Users/ahmedtamrawi/Desktop/test/java-docs/";
 	
 	private static Path OUTPUT_DIRECTORY_PATH;
+	
+	private static FileWriter writer;
+	
+    public static LanguageVersion languageVersion() {
+        return LanguageVersion.JAVA_1_5;
+    }
 
     public static boolean start(RootDoc rootDoc) {
+    	try {
+			writer = new FileWriter("/Users/ahmedtamrawi/Desktop/test/log.txt");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	write("1");
     	prepareOutputDirectoryStructure();
-    	
-    	StringWriter stringWriter = new StringWriter();
-    	 JsonWriter jsonWriter = Json.createWriter(stringWriter);
-    	 jsonWriter.writeObject(Json.createObjectBuilder().build());
-    	 jsonWriter.close();
-        File file = new File("/Users/ahmedtamrawi/Desktop/test/doclet-methodlist.csv");
-        try {
-            PrintWriter out = new PrintWriter(file);
-            try {
-                writeTo(out, rootDoc);
-                if (out.checkError())
-                    return false;
-            } finally {
-                out.close();
-            }
-        } catch (IOException ex) {
-            throw new RuntimeException(ex);
-        }
+    	write("2");
+    	generateDocumentationForJavaClasses(rootDoc);
+    	write("3");
+    	try {
+			writer.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
         return true;
+    }
+    
+    private static void write(String message) {
+    	try {
+			writer.write(message + "\n");
+			writer.flush();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
     
     private static void prepareOutputDirectoryStructure() {
@@ -46,6 +59,7 @@ public final class JSONDoclet {
     		try {
 				FileUtils.cleanDirectory(outputDirectoryFile);
 			} catch (IOException e) {
+				write("error3");
 				System.err.println("Error cleaning the output directory: " + OUTPUT_DIRECTORY_LOCATION);
 			}
     	}else {
@@ -53,59 +67,49 @@ public final class JSONDoclet {
     	}
     	OUTPUT_DIRECTORY_PATH = outputDirectoryFile.toPath();
     }
-
-    public static LanguageVersion languageVersion() {
-        return LanguageVersion.JAVA_1_5;
+    
+    private static void generateDocumentationForJavaClasses(RootDoc rootDoc) {
+    	ClassDoc[] classDocs = rootDoc.classes();
+    	for(ClassDoc classDoc: classDocs) {
+    		generateDocumentationForJavaClass(classDoc);
+    	}
     }
-
-    private static void writeTo(PrintWriter out, RootDoc rootDoc) {
-        // header
-        out.println(join(new String[]{"class", "package", "FQCN", "method", "modifiers",
-                                      "synthetic?", "parameters", "return type", "since"}));
-        // body
-        for (ClassDoc classDoc : rootDoc.classes()) {
-        	if(!classDoc.name().equals("XSSFWorkbook")) {
-        		continue;
-        	}
-            String[] a = new String[12];
-            Arrays.fill(a, "");
-            int i = -1;
-            a[++i] = classDoc.name();
-            a[++i] = classDoc.containingPackage().name();
-            a[++i] = classDoc.qualifiedName();
-            a[++i] = classDoc.getRawCommentText();
-            a[++i] = "";
-            a[++i] = "";
-            a[++i] = "";
-            a[++i] = "";
-            a[++i] = "";
-            out.println(join(a));
-        }
+    
+    private static void generateDocumentationForJavaClass(ClassDoc classDoc) {
+    	String qualifiedClassName = classDoc.qualifiedName();
+    	JSONObject classJSONObject = new JSONObject();
+    		JSONObject classProperties = new JSONObject();
+    		classProperties.put("comment1", classDoc.commentText());
+    		classProperties.put("comment2", classDoc.getRawCommentText());
+    	
+    			JSONObject fieldsJSONObject = new JSONObject();
+	    		FieldDoc[] fieldDocs = classDoc.fields();
+	    		for(FieldDoc fieldDoc: fieldDocs) {
+	    			fieldsJSONObject.put(fieldDoc.name(), fieldDoc.getRawCommentText());
+	    		}
+	    		classProperties.put("fields", fieldsJSONObject);
+    		
+	    		JSONObject methodsJSONObject = new JSONObject();
+	    		MethodDoc[] methodDocs = classDoc.methods();
+	    		for(MethodDoc methodDoc: methodDocs) {
+	    			methodsJSONObject.put(methodDoc.signature(), methodDoc.getRawCommentText());
+	    		}
+	    		classProperties.put("methods", methodsJSONObject);
+    	
+    		classJSONObject.put(qualifiedClassName, classProperties);
+    	saveJSONClassDocumentation(qualifiedClassName, classJSONObject);
     }
-
-    private static String parseSince(Doc... docs) {
-        for (Doc doc : docs) {
-            String s = join(doc.tags("since")).replace(",", "");
-            if (s.contains("@since:"))
-                s = s.replaceAll("@since:", "");
-            s = s.trim();
-            if (!s.isEmpty())
-                return s;
-        }
-        return "";
-    }
-
-    private static String quote(Object o) {
-        return String.format("\"%s\"", o);
-    }
-
-    private static <T> String join(T[] a) {
-        if (a.length == 0)
-            return "";
-        StringBuilder s = new StringBuilder(String.valueOf(a[0]));
-        for (int i = 1; i < a.length; i++)
-            s.append(",").append(a[i]);
-        return s.toString();
+    
+    private static void saveJSONClassDocumentation(String qualifiedClassName, JSONObject jsonObject) {
+    	File outputFile = OUTPUT_DIRECTORY_PATH.resolve(qualifiedClassName + ".json").toFile();
+        try {
+        	FileWriter file = new FileWriter(outputFile);
+            file.write(jsonObject.toString());
+			file.flush();
+			file.close();
+		} catch (IOException e) {
+			System.err.println("Error writing the JSON file for class: " + qualifiedClassName);
+		}
     }
 
 }
